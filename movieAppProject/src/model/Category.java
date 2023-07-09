a6bf5f41118d66ea3cb125459ea82f9020c3a17b
Gizot <tastangizem@gmail.com>
2023-07-09 19:06:38 +0300

    category model created

--- movieAppProject/src/model/Category.java
+++ movieAppProject/src/model/Category.java
@@ -1,4 +1,35 @@
 package model;
 
 public class Category {
+    private  String categoryName;
+    private  String filmCount;
+
+    public Category(String categoryName, String filmCount) {
+        this.categoryName = categoryName;
+        this.filmCount = filmCount;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getFilmCount() {
+        return filmCount;
+    }
+
+    public void setFilmCount(String filmCount) {
+        this.filmCount = filmCount;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "categoryName='" + categoryName + '\'' +
+                ", filmCount='" + filmCount + '\'' +
+                '}';
+    }
 }
