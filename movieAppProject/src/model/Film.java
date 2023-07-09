72cc5fa1c4be7270dfd2ec5be5276f6f573a8366
Gizot <tastangizem@gmail.com>
2023-07-09 19:54:03 +0300

    movieDuration and films of field created

--- movieAppProject/src/model/Film.java
+++ movieAppProject/src/model/Film.java
@@ -1,4 +1,13 @@
 package model;
 
+import java.util.List;
+
 public class Film {
+    private String filmName;
+    private String director;
+    private Double imdbPoint;
+    private List<Category> filmCategory;
+    private List<Platform> filmPlatfom;
+
+
 }
