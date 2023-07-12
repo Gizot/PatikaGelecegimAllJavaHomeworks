81dcccc3b331ca566f873ec332f31782d5314117
Gizot <tastangizem@gmail.com>
2023-07-12 06:53:17 +0300

    lesson practice patikaDance creating

---
+++ patikaDans/src/model/LectureScheduleTime.java
@@ -0,0 +1,36 @@
+package model;
+
+public class LectureScheduleTime {
+    private Day day;
+
+    private String time;
+
+    public LectureScheduleTime(Day day, String time) {
+        this.day = day;
+        this.time = time;
+    }
+
+    public Day getDay() {
+        return day;
+    }
+
+    public void setDay(Day day) {
+        this.day = day;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return "LectureScheduleTime{" +
+                "day=" + day +
+                ", time='" + time + '\'' +
+                '}';
+    }
+}
