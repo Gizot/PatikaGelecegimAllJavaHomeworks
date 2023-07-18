fddbe6d46ccfae72270762b9be74e935af94cf86
Gizot <tastangizem@gmail.com>
2023-07-18 20:43:05 +0300

    power calculate with recursive methods

---
+++ PowerCalculateWithRecursiveMethod/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+                Scanner scan = new Scanner(System.in);
+
+                System.out.print("Taban değerini giriniz: ");
+                int base = scan.nextInt();
+                System.out.print("Üs değerini giriniz: ");
+                int exponent = scan.nextInt();
+
+                int result = power(base, exponent);
+
+                System.out.println("Result " + result);
+            }
+
+            static int power(int base, int exponent) {
+                if (exponent == 0) {
+                    return 1;
+                } else {
+                    return base * power(base, exponent - 1);
+                }
+            }
+        }
+
