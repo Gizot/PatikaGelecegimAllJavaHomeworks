38d178879b5ed67a17e8035e02f75d0d5cad0358
Gizot <tastangizem@gmail.com>
2023-07-18 15:06:01 +0300

    diamonds consist of stars

---
+++ diamondConsistOfStars/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Elmas deseninin yüksekliğini girin: ");
+        int height = input.nextInt();
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < height - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = height - 2; i >= 0; i--) {
+            for (int j = 0; j < height - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
