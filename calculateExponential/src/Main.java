e84bb47fa5b3f6a086f8124d8b131c5ac770301e
Gizot <tastangizem@gmail.com>
2023-07-18 14:45:36 +0300

    project done

---
+++ calculateExponential/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the base number: ");
+        int base = scanner.nextInt();
+
+        System.out.print("Enter the exponent number: ");
+        int exponent = scanner.nextInt();
+
+        int result = 1;
+
+        for (int i = 1; i <= exponent; i++) {
+            result *= base;
+        }
+
+        System.out.println(base + "^" + exponent + " = " + result);
+    }
+}
