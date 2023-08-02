9f0b652d97d4dd89bf21feb586f6f846bf0bd27f
Gizot <tastangizem@gmail.com>
2023-08-02 14:48:14 +0300

    BaseEntity class done

---
+++ pokemonMvn/src/main/java/com/allianz/pokemonmvn/PokemonMvnApplication.java
@@ -0,0 +1,13 @@
+package com.allianz.pokemonmvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonMvnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonMvnApplication.class, args);
+    }
+
+}
---
+++ pokemonMvn/src/test/java/com/allianz/pokemonmvn/PokemonMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allianz.pokemonmvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonMvnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
